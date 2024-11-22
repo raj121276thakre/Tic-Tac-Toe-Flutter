b7d799feb84d17ace8983dcce753ac93a52c3988
raj121276thakre <raj121276thakre@gmail.com>
2024-11-22 22:21:56 +0530

    7. WelcomePage (onBoarding) design done.

--- lib/Controller/SplashController.dart
+++ lib/Controller/SplashController.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:tik_tac_toe_multiplayer/Pages/RoomPage/RoomPage.dart';
+import 'package:tik_tac_toe_multiplayer/Pages/Welcome/WelcomePage.dart';
 
 class SplashController extends GetxController {
   @override
@@ -10,6 +11,6 @@ class SplashController extends GetxController {
 
   Future<void> splashHandle() async {
     await Future.delayed(const Duration(seconds: 3));
-    Get.offAll(const RoomPage());
+    Get.offAll(const WelcomePage());
   }
 }
---
+++ lib/Pages/Welcome/WelcomePage.dart
@@ -0,0 +1,101 @@
+import 'package:concentric_transition/page_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tik_tac_toe_multiplayer/Configs/AssetsPath.dart';
+import 'package:tik_tac_toe_multiplayer/Pages/RoomPage/RoomPage.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var pages = [
+      // 1
+      Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(ImagePath.welcome1),
+            SizedBox(height: 20,),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            Text(
+              "Most fun game now available on your smartphone device!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+
+      // 2
+      Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(ImagePath.welcome2),
+            SizedBox(height: 20,),
+            Text(
+              "Compete",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            Text(
+              "Play online with your friends and top the leaderboard!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+
+      // 3
+      Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 60,),
+            Image.asset(ImagePath.welcome3),
+            SizedBox(height: 20,),
+            Text(
+              "Scoreboard",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            Text(
+              "Earn points for each game and make your way to top the scoreboard!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+            SizedBox(height: 60,),
+            OutlinedButton(
+
+                onPressed: () {
+                  Get.offAll(const RoomPage());
+                },
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(color: Colors.black),
+                ))
+          ],
+        ),
+      ),
+    ];
+    return Scaffold(
+      body: ConcentricPageView(
+        colors: [Theme.of(context).colorScheme.surface, Theme.of(context).colorScheme.surface, Theme.of(context).colorScheme.surface,],
+        itemCount: 3,
+        physics: BouncingScrollPhysics(),
+        onFinish: () {
+          Get.offAll(const RoomPage());
+        },
+        itemBuilder: (int index) {
+          return pages[index];
+        },
+      ),
+    );
+  }
+}
