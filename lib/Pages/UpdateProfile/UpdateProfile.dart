e3ad8c995f21d3c8ed2ea09fa10bad63a2386783
raj121276thakre <raj121276thakre@gmail.com>
2024-11-23 23:20:49 +0530

    16. Save Button overflow error Solved in UpdateProfile.dart.

--- lib/Pages/UpdateProfile/UpdateProfile.dart
+++ lib/Pages/UpdateProfile/UpdateProfile.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tik_tac_toe_multiplayer/Components/PrimaryButtonWithIcon.dart';
@@ -18,124 +18,287 @@ class UpdateProfile extends StatelessWidget {
     TextEditingController nameController = TextEditingController();
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Obx(
-                        () => imagePath == ""
-                            ? Container(
-                                width: 180,
-                                height: 180,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(40),
-                                ),
-                                child: const Icon(
-                                  Icons.add_a_photo_outlined,
-                                  size: 50,
-                                  color: Colors.grey,
-                                ),
-                              )
-                            : Container(
-                                width: 180,
-                                height: 180,
-                                decoration: BoxDecoration(
-                                  // color: Colors.red,
-                                  borderRadius: BorderRadius.circular(40),
-                                ),
-                                child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(40),
-                                    child: Image.file(
-                                      File(imagePath.value),
-                                      fit: BoxFit.cover,
-                                    )),
-                              ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        children: [
-                          InkWell(
-                            //camera
-                            onTap: () async {
-                              imagePath.value = await profileController
-                                  .pickImage(ImageSource.camera);
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: SvgPicture.asset(IconsPath.cameraIcon),
+      resizeToAvoidBottomInset: true,
+      // Allows resizing when the keyboard appears
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Obx(
+                              () => imagePath.value == ""
+                                  ? Container(
+                                      width: 180,
+                                      height: 180,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(40),
+                                      ),
+                                      child: const Icon(
+                                        Icons.add_a_photo_outlined,
+                                        size: 50,
+                                        color: Colors.grey,
+                                      ),
+                                    )
+                                  : Container(
+                                      width: 180,
+                                      height: 180,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(40),
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(40),
+                                        child: Image.file(
+                                          File(imagePath.value),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          InkWell(
-                            //gallery
-                            onTap: () async {
-                              imagePath.value = await profileController
-                                  .pickImage(ImageSource.gallery);
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: SvgPicture.asset(IconsPath.galleryIcon),
+                            SizedBox(
+                              width: 20,
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                            Column(
+                              children: [
+                                InkWell(
+                                  onTap: () async {
+                                    imagePath.value = await profileController
+                                        .pickImage(ImageSource.camera);
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child:
+                                        SvgPicture.asset(IconsPath.cameraIcon),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                InkWell(
+                                  onTap: () async {
+                                    imagePath.value = await profileController
+                                        .pickImage(ImageSource.gallery);
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child:
+                                        SvgPicture.asset(IconsPath.galleryIcon),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        TextFormField(
+                          controller: nameController,
+                          decoration:
+                              InputDecoration(hintText: "Enter your name"),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "You can change these details later from the profile page. Don’t worry.",
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(
+                                  color: Theme.of(context).colorScheme.primary),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(
-                    height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Obx(
+                    () => profileController.isLoading.value
+                        ? CircularProgressIndicator()
+                        : PrimaryButtonWithIcon(
+                            buttonText: "Save",
+                            onTap: () {
+                              profileController.updateProfile(
+                                  nameController.text, imagePath.value);
+                            },
+                            iconPath: IconsPath.saveIcon),
                   ),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: InputDecoration(hintText: "Enter your name"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "You can change these details later  from profile page. don’t worry",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                ],
-              ),
-              Obx(
-                () => profileController.isLoading.value
-                    ? CircularProgressIndicator()
-                    : PrimaryButtonWithIcon(
-                        buttonText: "Save",
-                        onTap: () {
-                          profileController.updateProfile(
-                              nameController.text, imagePath.value);
-                        },
-                        iconPath: IconsPath.saveIcon),
-              )
-            ],
-          ),
+                ),
+                SizedBox(height: 20),
+              ],
+            );
+          },
         ),
       ),
     );
   }
+
+
 }
+
+
+
+
+
+
+
+// class UpdateProfile extends StatelessWidget {
+//   const UpdateProfile({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     ProfileController profileController = Get.put(ProfileController());
+//     RxString imagePath = "".obs;
+//     TextEditingController nameController = TextEditingController();
+//
+//     return Scaffold(
+//
+//       body: Padding(
+//         padding: const EdgeInsets.all(20),
+//         child: SafeArea(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Column(
+//                 children: [
+//                   SizedBox(
+//                     height: 30,
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       Obx(
+//                         () => imagePath == ""
+//                             ? Container(
+//                                 width: 180,
+//                                 height: 180,
+//                                 decoration: BoxDecoration(
+//                                   color: Colors.white,
+//                                   borderRadius: BorderRadius.circular(40),
+//                                 ),
+//                                 child: const Icon(
+//                                   Icons.add_a_photo_outlined,
+//                                   size: 50,
+//                                   color: Colors.grey,
+//                                 ),
+//                               )
+//                             : Container(
+//                                 width: 180,
+//                                 height: 180,
+//                                 decoration: BoxDecoration(
+//                                   // color: Colors.red,
+//                                   borderRadius: BorderRadius.circular(40),
+//                                 ),
+//                                 child: ClipRRect(
+//                                     borderRadius: BorderRadius.circular(40),
+//                                     child: Image.file(
+//                                       File(imagePath.value),
+//                                       fit: BoxFit.cover,
+//                                     )),
+//                               ),
+//                       ),
+//                       SizedBox(
+//                         width: 20,
+//                       ),
+//                       Column(
+//                         children: [
+//                           InkWell(
+//                             //camera
+//                             onTap: () async {
+//                               imagePath.value = await profileController
+//                                   .pickImage(ImageSource.camera);
+//                             },
+//                             child: Container(
+//                               padding: EdgeInsets.all(10),
+//                               decoration: BoxDecoration(
+//                                   color: Theme.of(context).colorScheme.primary,
+//                                   borderRadius: BorderRadius.circular(10)),
+//                               child: SvgPicture.asset(IconsPath.cameraIcon),
+//                             ),
+//                           ),
+//                           SizedBox(
+//                             height: 20,
+//                           ),
+//                           InkWell(
+//                             //gallery
+//                             onTap: () async {
+//                               imagePath.value = await profileController
+//                                   .pickImage(ImageSource.gallery);
+//                             },
+//                             child: Container(
+//                               padding: EdgeInsets.all(10),
+//                               decoration: BoxDecoration(
+//                                   color:
+//                                       Theme.of(context).colorScheme.secondary,
+//                                   borderRadius: BorderRadius.circular(10)),
+//                               child: SvgPicture.asset(IconsPath.galleryIcon),
+//                             ),
+//                           ),
+//                         ],
+//                       )
+//                     ],
+//                   ),
+//                   SizedBox(
+//                     height: 40,
+//                   ),
+//                   TextFormField(
+//                     controller: nameController,
+//                     decoration: InputDecoration(hintText: "Enter your name"),
+//                   ),
+//                   SizedBox(
+//                     height: 20,
+//                   ),
+//                   Text(
+//                     "You can change these details later  from profile page. don’t worry",
+//                     textAlign: TextAlign.center,
+//                     style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+//                         color: Theme.of(context).colorScheme.primary),
+//                   ),
+//                 ],
+//               ),
+//
+//               Obx(
+//                 () => profileController.isLoading.value
+//                     ? CircularProgressIndicator()
+//                     : PrimaryButtonWithIcon(
+//                         buttonText: "Save",
+//                         onTap: () {
+//                           profileController.updateProfile(
+//                               nameController.text, imagePath.value);
+//                         },
+//                         iconPath: IconsPath.saveIcon),
+//               ),
+//
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
